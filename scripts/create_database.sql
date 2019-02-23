3da6fe44f5dc862038920cde79477940cde97322
fmaj7-dev <eturegano@gmail.com>
2019-02-23 23:28:54 +0100

    fill_database script added

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -2,32 +2,36 @@
 create DATABASE language_flashcards;
 use language_flashcards;
 
-CREATE TABLE `vocabulary` (
-  `id` int(11) NOT NULL,
-  `lang_a` text COLLATE utf8_bin NOT NULL,
-  `lang_b` text COLLATE utf8_bin NOT NULL,
-  `level` int(11) NOT NULL,
+CREATE TABLE `languages` (
+  `id` tinyint NOT NULL,
+  `name` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE `languages` (
+CREATE TABLE `vocabulary` (
   `id` int(11) NOT NULL,
-  `name` text COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
+  `word_a` text COLLATE utf8_bin NOT NULL,
+  `word_b` text COLLATE utf8_bin NOT NULL,
+  `language_a` tinyint NOT NULL,
+  `language_b` tinyint NOT NULL,
+  `level` tinyint NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`language_a`) REFERENCES languages(`id`),
+  FOREIGN KEY(`language_b`) REFERENCES languages(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` text COLLATE utf8_bin NOT NULL,
   `email` text COLLATE utf8_bin NOT NULL,
-  `native_language` int(11) NOT NULL,
+  `native_language` tinyint NOT NULL,
   FOREIGN KEY(`native_language`) REFERENCES languages(`id`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `user_language` (
   `user_id` int(11) NOT NULL,
-  `language_id` int(11) NOT NULL,
+  `language_id` tinyint NOT NULL,
   FOREIGN KEY(`user_id`) REFERENCES users(`id`),
   FOREIGN KEY(`language_id`) REFERENCES languages(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- scripts/fill_database.sql
+++ scripts/fill_database.sql
@@ -1,1 +1,30 @@
 
+INSERT INTO `languages` (`id`, `name`) VALUES
+    (1, 'Spanish'),
+    (2, 'French');
+
+INSERT INTO `vocabulary` (`id`, `word_a`, `word_b`, `language_a`, `language_b`, `level`) VALUES
+    (1, 'tôt /to/', 'temprano\r\n', 2, 1, 2), 
+    (2, 'en retard /ʀ(ə)taʀ/', 'tarde, con retraso\r\n', 2, 1, 2), 
+    (3, 'avant (de) /avɑ̃/', 'antes (de)\r\n', 2, 1, 2), 
+    (4, 'après /apʀɛ/', 'después\r\n', 2, 1, 1);
+
+INSERT INTO `users` (`id`, `name`, `email`, `native_language`) VALUES
+    (1, 'Enrique', 'test@test.com', 1),
+    (2, 'Patri', 'test@test.com', 1);
+
+INSERT INTO `user_language` (`user_id`, `language_id`) VALUES
+    (1, 2),
+    (2, 2);
+
+INSERT INTO `guess` (`user_id`, `vocabulary_id`, `a2b_ok`, `a2b_ko`, `b2a_ok`, `b2a_ko`) VALUES
+    (1, 1, 0, 0, 0, 0),
+    (1, 2, 0, 0, 0, 0),
+    (1, 3, 0, 0, 0, 0),
+    (1, 4, 0, 0, 0, 0),
+    (2, 1, 0, 0, 0, 0),
+    (2, 2, 0, 0, 0, 0),
+    (2, 3, 0, 0, 0, 0),
+    (2, 4, 0, 0, 0, 0);
+
+
